256192cdb53d75dfc0425286afc815c9a97494a5
Pliushchou Alexander <aap.sasha@gmail.com>
2022-07-08 17:36:08 +0300

    first

---
+++ 2019task17/2019task17.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+using namespace std;
+int delete_chet(int a) {
+	if (a == 0) {
+		return 0;
+	}
+	else if (a % 2 == 0) {
+		return delete_chet(a / 10);
+	}
+	else {
+		return a%10 + 10 * delete_chet(a / 10);
+	}
+}
+int main()
+{
+	int n;
+	cin >> n;
+	cout << delete_chet(n);
+}
+
---
+++ lesson1/lesson1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int x, y=1;
+    cin >> x;
+    if (x < 0) {
+        cout << "error";
+    }
+    else {
+        for (int i = 2; i <= x; i++)
+        {
+            y *= i;
+        }
+        cout << y;
+    }
+}
